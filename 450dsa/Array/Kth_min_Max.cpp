7df416402d76c44a32835975c171fc5de5633299
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-06-08 23:36:11 +0530

    450

---
+++ 450dsa/Array/Kth_min_Max.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+   
+int Kth_max__min(int arr,int n,int k)
+{
+    sort(arr,arr+n);
+    
+    int min=arr[n-k];
+    int max=arr[n-k]
+}
+    
+
+
+int main()
+{
+    int arr[] = {7 10 4 3 20 15};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int result=
+    cout<<result<<endl;
+    
+
+    return 0;
+}
+
