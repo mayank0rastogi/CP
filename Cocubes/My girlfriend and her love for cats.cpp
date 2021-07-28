876059cf2f47ab23dc6bd79f5a8c8edf552b683e
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-07-28 23:26:11 +0530

    Update

--- Cocubes/My girlfriend and her love for cats.cpp
+++ Cocubes/My girlfriend and her love for cats.cpp
@@ -24,51 +24,34 @@ Sample Input
 Sample Output
 15
 */
+
 #include <iostream>
-
 #include <algorithm>
-
 using namespace std;
 
 int main()
 
 {
-
-    long int n,i;
-
+   long int n,i;
     cin>>n;
-
     long int s[n],c[n];
 
-    for(i=0;i<n;i++)
-
-    {
-
-        cin>>s[i];
-
-    }
+   for(i=0;i<n;i++)
+   {
+         cin>>s[i];
+   }
 
     for(i=0;i<n;i++)
-
-    {
-
-        cin>>c[i];
-
-    }
-
-    sort(s,s+n);
-
-    sort(c,c+n);
-
-    long int sum=0;
-
-    for(i=0;i<n;i++)
-
-    {
-
-        sum=sum+(s[i]*c[i]);
-
-    }
+     {
+       cin>>c[i];
+     }
+     sort(s,s+n);
+     sort(c,c+n);
+     long int sum=0;
+     for(i=0;i<n;i++)
+      {
+         sum=sum+(s[i]*c[i]);
+      }
 
     cout<<sum;
 
