f75abc2af4654467146e2a2b5e13550363da2177
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-08-14 04:35:52 +0530

    696

---
+++ Leetcode/STRING/696. Count Binary Substrings.cpp
@@ -0,0 +1,81 @@
+/*
+Give a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+Substrings that occur multiple times are counted the number of times they occur.
+
+ 
+
+Example 1:
+
+Input: s = "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+Notice that some of these substrings repeat and are counted the number of times they occur.
+Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+Example 2:
+
+Input: s = "10101"
+Output: 4
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either '0' or '1'.
+*/
+
+
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        
+        int n=s.length();
+        int ret=0;
+        int curr=1,prev=0;
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                curr++;
+            }
+            
+            else
+            {
+                ret+=min(curr,prev);
+                prev=curr;
+                curr=1;
+            }
+        }
+        
+        ret+=min(curr,prev);//for last occurence
+        
+//         int n=s.length();
+//         int res=0;
+//         int crr=0,cnt2=0;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i]=='0')
+//                 cnt1++;
+            
+//             else
+//             {
+//                 cnt2++;
+//             }
+            
+//             if(cnt1==cnt2)
+//             {
+//                 res++;
+//             }
+//         }
+        
+//         if(cnt1!=cnt2)
+//         {
+//             return -1;
+//         }
+        
+//         return res;
+        return ret;
+    }
+};
