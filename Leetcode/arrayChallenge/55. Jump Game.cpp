64d0ec3f727395b7f086b5f76b0023f6fea41d9d
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-08-15 01:44:07 +0530

    55

---
+++ Leetcode/arrayChallenge/55. Jump Game.cpp
@@ -0,0 +1,59 @@
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// 0 <= nums[i] <= 105
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int i=0;
+        int max_reach=nums[0];
+        
+//         if(nums.size()==1)
+//         {
+//             return true;
+//         }
+        
+        if(max_reach>=nums.size()-1)
+        {
+            return true;
+        }
+        
+        while(i<=max_reach)
+        {
+            if(nums[i]+i>max_reach)
+            {
+                max_reach=nums[i]+i;
+            }
+            i++;
+            
+            if(max_reach>=nums.size()-1)
+            {
+                return true;
+            }
+        }
+       return false;
+            
+      
+    }
+};
