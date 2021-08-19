44fe7c7adb3ce1d13c1842904a10a5bf26f7a9e8
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-08-19 22:27:25 +0530

    1768

---
+++ Leetcode/Two Pointers/1768. Merge Strings Alternately.cpp
@@ -0,0 +1,78 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b 
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+// Example 3:
+
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q 
+// merged: a p b q c   d
+ 
+
+// Constraints:
+
+// 1 <= word1.length, word2.length <= 100
+// word1 and word2 consist of lowercase English letters.
+
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+       
+       int i=0,j=0;
+        string temp="",ans="";
+      
+      ///if any of the string is only one word for this i have to write the logic 
+        
+        if(word1.size()==1)
+        {
+            temp+=word1[0];
+            temp+=word2;
+            return temp;
+        }
+        
+        if(word2.size()==1)
+        {
+            temp+=word1[0];
+            temp+=word2;
+            temp+=word1.substr(1);
+            return temp;
+        }
+        // if both the string have more than 1 no of words for this time i have to write the logic
+        while(i<word1.size() || j<word2.size())
+        {
+            if(i<word1.size())
+            {
+                ans+=word1[i];
+            }
+            if(j<word2.size())
+            {
+                ans+=word2[j];
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
