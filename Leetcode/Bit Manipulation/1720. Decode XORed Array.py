9044f9aeca4a380ce418a51706d82867a6762ac2
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-08-24 02:04:00 +0530

    1720(py)

--- Leetcode/Bit Manipulation/1720. Decode XORed Array.py
+++ Leetcode/Bit Manipulation/1720. Decode XORed Array.py
@@ -2,8 +2,14 @@ class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
         ans=[]
         ans.append(first)
-        n=len(encoded)
-        for i in range(0,n):
-            ans.append(encoded[i]^ans[i])
-        return ans
+        # n=len(encoded)
+        # for i in range(0,n):
+        #     ans.append(encoded[i]^ans[i])
+        # return ans
         
+        
+        #2nd method uncomment above see the result
+        for i in encoded:
+            first=first^i
+            ans.append(first)
+        return ans
