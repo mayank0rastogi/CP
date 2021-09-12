3d3b9448d77d1c5bfd3b8d0224dd4f94cd34a90e
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2021-09-12 10:36:25 +0530

    199

---
+++ Leetcode/Tree/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,59 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int>res;
+        recursion(root,0,res);
+        return res;
+        
+    }
+    
+    void recursion(TreeNode* root,int level,vector<int>&res)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(res.size()==level)
+           {
+               res.push_back(root->val);
+           }
+        recursion(root->right,level+1,res);
+        recursion(root->left,level+1,res);
+    }
+};
