0d0af3d31f9bba56924b537cfd30ebed32e00ccb
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-01-23 23:53:12 +0530

    41A

---
+++ Codeforces/Div A/41A.py
@@ -0,0 +1,18 @@
+s1=input()
+s2=input()
+
+if s1==s2[::-1]:
+    print("YES")
+else:
+    print("NO")
+# print(s2[:-3])
+
+
+
+#Python string slicing concept..
+# String ='ASTRING'
+
+# # Using indexing sequence
+# print(String[:3])
+# print(String[0:5:2])
+# print(String[-1:-34:-2])
