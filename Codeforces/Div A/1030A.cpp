2fa3c4ed94e50676a82a6b284435b421de14b23a
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-01-26 23:07:53 +0530

    1030A

---
+++ Codeforces/Div A/1030A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+   int problem;
+    
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>problem;
+        if(problem==1)
+        {
+           cout<<"HARD"<<endl;
+           return 0;
+        }
+        
+        
+    }
+   
+        cout<<"EASY"<<endl;
+    
+    
+
+    return 0;
+}
