8e98833a93ac0cda6fe3cb2289644d2420c6f773
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-01-30 23:35:28 +0530

    AVGOF3

---
+++ CC/START23C/AVGOF3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+int p,q,r;
+cin>>t;
+
+while(t--)
+{
+    int n;
+    cin>>n;
+   
+    
+    cout<<n-1<<" "<<n<<" "<<n+1<<endl;
+}
+	return 0;
+}
