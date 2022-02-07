c5f860f93e58278caef5c4cf379e4a68e3493243
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-02-07 23:52:22 +0530

    705A

---
+++ Codeforces/Div A/705A.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"I hate ";
+    for(int i=1;i<n;i++)
+    {
+        if(i%2==1)
+        {
+            cout<<"that I love ";
+        }
+        else
+        {
+            cout<<"that I hate ";
+        }
+        
+    }
+    cout<<"it";
+   
+
+    return 0;
+}
