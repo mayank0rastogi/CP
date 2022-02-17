e0cf8a54120678282b5c927ba552f21e05324de3
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-02-17 23:43:18 +0530

    WCC

---
+++ CC/February Long 2022 - II/WCC.py
@@ -0,0 +1,21 @@
+for _ in range(int(input())):
+    x=int(input())
+    s=input()
+    Carlsen=0
+    Chef=0
+    draw=0
+    for i in range(0,len(s)):
+        if s[i]=="C":
+            Carlsen+=1
+        elif s[i]=="N":
+            Chef+=1
+        else:
+            draw+=1
+    Carlsen=2*(Carlsen+draw)
+    Chef=2*(Chef+draw)
+    if Carlsen>Chef:
+        print(60*x)
+    elif Chef>Carlsen:
+        print(40*x)
+    else:
+        print(55*x)
