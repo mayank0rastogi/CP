1299231bfdedd535b7e743677a5bd14ab6d70bcf
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-02-22 23:42:14 +0530

    NOFIX

---
+++ CC/February Long 2022 - II/NOFIX.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n=int(input())
+    arr=[int(x) for x in input().split()]
+    c=0
+    k=0
+    for i in range(0,len(arr)):
+        k=i+c+1
+        if k==arr[i]:
+            c=c+1
+    print(c)
+# Approach-->
+# 1)in this basically every matched  operation we find it means other elemnets of arr is disturb 
+# due to this we check for same arr just by adding that matched operation into that ith index 
+            
