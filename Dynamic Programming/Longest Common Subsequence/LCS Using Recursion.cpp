89ca97c20d027270b3ab62f6f5dc3006854ec759
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-02-23 03:03:14 +0530

    Using Recursion

---
+++ Dynamic Programming/Longest Common Subsequence/LCS Using Recursion.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int LCS(string s1,string s2,int m,int n)
+{
+    
+    
+    //Base Condition
+    if(n==0 || m==0)
+    {
+        return 0;
+    }
+    
+    // choice diagram condition 
+    
+    if(s1[n-1]==s2[m-1])
+    {
+        return 1+LCS(s1,s2,n-1,m-1);
+    }
+    else
+    {
+        return max(LCS(s1,s2,n,m-1),LCS(s1,s2,n-1,m));
+    }
+    
+  
+    
+}
+
+int main()
+{
+    string s1="AGGTAB";
+    int n=s1.size();
+    string s2="GXTXAYB";
+    int m=s2.size();
+    cout<<LCS(s1,s2,m,n)<<endl;
+
+    return 0;
+}
