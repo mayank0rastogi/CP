22ae123ee023e8e8930f4081e382134b0e0265b1
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-02-26 17:24:23 +0530

    Top Down DP

---
+++ Dynamic Programming/Longest Common Subsequence/LCS Using Top Down DP.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCS(string s1,string s2,int n,int m,vector<vector<int>>dp)
+{
+    //base condition
+    for(int i=0;i<n+1;i++)
+    {
+        for(int j=0;j<m+1;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j]=0;
+            }
+        }
+    }
+    
+    
+    for(int i=1;i<n+1;i++)
+    {
+        for(int j=1;j<m+1;j++)
+        {
+            if(s1[i-1]==s2[j-1])
+            {
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            
+            else
+            {
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    
+    return dp[n][m];
+}
+
+int main()
+{
+    string s1="AGGTAB";
+    string s2="GXTXAYB";
+    int n=s1.size();
+    int m=s2.size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    
+    cout<<LCS(s1,s2,n,m,dp)<<endl;
+}
