a8c5f4b030475effe7cea4476830645f81a45ab1
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-03-14 23:56:29 +0530

    Leetcode Q

---
+++ Dynamic Programming/DP on Trees/543. Diameter of Binary Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int solve(TreeNode* root,int &res)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        int lh=solve(root->left,res);
+        int rh=solve(root->right,res);
+        
+         res=max(res,(lh+rh));
+           
+         return 1+ max(lh,rh);
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        int res=INT_MIN;
+        solve(root,res);
+        return res;
+        
+        
+    }
+};
