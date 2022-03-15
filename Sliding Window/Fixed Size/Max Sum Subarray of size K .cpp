f20875ad6ec06c502cdf0aa787ea594f9ec42e56
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-03-15 23:55:35 +0530

    Max Sum Subarray of size K

---
+++ Sliding Window/Fixed Size/Max Sum Subarray of size K .cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+class Solution{   
+public:
+    int maximumSumSubarray(int K, vector<int> &Arr , int N)
+    {
+          int i=0,j=0;
+          int sum=0;
+          int mx=INT_MIN;
+          while (j<N)
+          {
+            sum=sum+Arr[j]; // do calculation to reduse time complexity
+            if (j-i+1<K) 
+            {
+                j++; // increament j upto when the size of the size of window is not equal to required size
+            }
+            else if ((j-i+1)==K) // when sindow size hit to the required window size 
+            {
+              mx=max(mx,sum); // selecting ans from the candidates
+              sum=sum-Arr[i]; // start removing from the first
+              i++;
+              j++;
+            }
+          }
+        
+        return mx;
+    }
+    
+};
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
