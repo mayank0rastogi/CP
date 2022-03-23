658806d4f4bd6d33fadafc894efb79ffe95242e4
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-03-23 12:31:10 +0530

    Longest Substring With Without Repeating Chars

---
+++ Sliding Window/Variable Size/Longest Substring With Without Repeating Characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int mx=0;
+        int i=0,j=0;
+        unordered_map<char,int>ump;
+        while(j<s.size())
+        {
+            ump[s[j]]++;
+            
+            if(ump.size()>j-i+1)
+            {
+                j++;
+            }
+            
+            if(ump.size()==j-i+1)
+            {
+                mx=max(mx,j-i+1);
+                j++;
+            }
+            
+            else if(ump.size()<j-i+1)
+            {
+                while(ump.size()<j-i+1)
+                {
+                    ump[s[i]]--;
+                    
+                    if(ump[s[i]]==0)
+                    {
+                        ump.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return mx;
+    }
+};
