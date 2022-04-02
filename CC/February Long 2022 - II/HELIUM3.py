0a8e3e9744b553f9ceb44217e272befa94c6e8c2
Mayank Rastogi <60985136+mayank0rastogi@users.noreply.github.com>
2022-04-02 23:43:29 +0530

    Update HELIUM3.py

--- CC/February Long 2022 - II/HELIUM3.py
+++ CC/February Long 2022 - II/HELIUM3.py
@@ -1,7 +1,9 @@
 for _ in range(int(input())):
     a,b,x,y=list(map(int,input().split()))
     tot_units_power=x*y
+    
     moon_power=a*b
+    
     if tot_units_power>=moon_power:
         print("YES")
     else:
